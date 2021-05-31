9e68625f0d432f23fc3dc2da413b6e66f9705017
Yusuf <yusufsaquib@gmail.com>
2021-05-30 23:08:44 -0400

    added python script for emulator commands

--- firebase-start-emulators.ps1
+++
@@ -1,1 +0,0 @@
-firebase emulators:start --import=./firebase-dev --export-on-exit
---
+++ start-emulator.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import subprocess
+from types import new_class
+
+options = {
+    "a" : "auth",
+    # "db" : "database",
+    "f" : "firestore",
+    # "fn" : "functions",
+    "h" : "hosting",
+    "s" : "storage"
+    # "ps" : "pubsub"
+}
+
+firebase = "firebase"
+
+start = "emulators:start"
+import_dir = "--import=./firebase-dev/export"
+export_on_exit = "--export-on-exit"
+
+only_opt = []
+
+res_command = ["powershell.exe", firebase, start, import_dir, export_on_exit]
+
+argc = len(sys.argv)
+
+if(argc > 1):
+    for opt in sys.argv[1::]:
+        if opt not in options:
+            exit("\033[91mError: Invalid Argument \"" + str(opt) + "\"\033[0m")
+        only_opt += [options[opt]]
+
+    only_flag = "--only \"" + ",".join(only_opt) + "\""
+    res_command.append(only_flag)
+
+process = subprocess.Popen(res_command, stderr=sys.stderr, stdin=sys.stdin, stdout=sys.stdout)
+process.communicate()
+
+sys.exit(process.returncode)
