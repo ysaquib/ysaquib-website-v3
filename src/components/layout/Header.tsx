022f0d6972f201be45266270a9c3726cef89c4aa
Yusuf <yusufsaquib@gmail.com>
2021-06-07 20:01:26 -0400

    exported firebase data & added header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,6 +4,7 @@
  */
 
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 
 let default_data = require('../../default_data.json');
@@ -13,12 +14,19 @@ const Header : FC = () =>
     return (
         <header id="header">
             <div className="header_wrapper">
-                
+                <ol>
+                    {default_data.header.sections.map(([title, path] : [string, string]) => {
+                        return (<li className="header_item">
+                                    <a href={path} className="header_link">
+                                        {title}
+                                    </a>
+                                </li>)
+                    })}
+                </ol>
             </div>
         </header>
     );
     
 }
 
-Header.defaultProps = {isOnHome: true};
 export default Header;
