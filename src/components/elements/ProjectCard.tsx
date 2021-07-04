e01833107edd07a8360767c93144561477730372
Yusuf <yusufsaquib@gmail.com>
2021-07-04 05:25:02 -0400

    changed colors and line heights for better reading

--- src/components/elements/ProjectCard.tsx
+++ src/components/elements/ProjectCard.tsx
@@ -4,6 +4,7 @@
  */
 
 import React, {FC} from 'react';
+import { useHistory } from 'react-router-dom';
 import { ProjectData } from '../../store/types/dataTypes';
 import { IconArticle, IconGithub, IconLaunch } from './Icons';
 
@@ -16,6 +17,12 @@ interface CardProps extends ProjectData
 const ProjectCard : FC<CardProps> = ({children, className, order, ...projectData}) => 
 {
     const featured_order : number = 0;
+    const history = useHistory();
+
+    const currentURL = new URL(window.location.href);
+    const blogURL = new URL(projectData.project_blog && projectData.project_blog !== "" ? projectData.project_blog : "https://www.yusufsaquib.com/");
+    const isOriginSame = blogURL.origin === currentURL.origin;
+
     /**
      * Create the "Featured Project" tag that will only be applied to the 
      * featured project (any project with the 'featured' className)
@@ -77,9 +84,9 @@ const ProjectCard : FC<CardProps> = ({children, className, order, ...projectData
                     {featured_tag}
                     {wip_tag}
                     <ul className="links_list">
-                        {projectData.project_blog && <li className="links_item svg_icon" key="blog" title="Read More">{IconArticle}</li>}
-                        {projectData.project_github && <li className="links_item svg_icon" key="github" title="View Github Repo">{IconGithub}</li>}
-                        {projectData.project_url && <li className="links_item svg_icon" key="demo" title="View Demo">{IconLaunch}</li>}
+                        {projectData.project_blog && <li className="links_item svg_icon" key="blog" title="Read More" onClick={() => isOriginSame ? history.push(blogURL.pathname) : window.open(projectData.project_blog, "_blank", "noopener noreferrer")}>{IconArticle}</li>}
+                        {projectData.project_github && <li className="links_item svg_icon" key="github" title="View Github Repo" onClick={()=>window.open(projectData.project_github, "_blank", "noopener noreferrer")}>{IconGithub}</li>}
+                        {projectData.project_url && <li className="links_item svg_icon" key="demo" title="View Demo" onClick={()=>window.open(projectData.project_url, "_blank", "noopener noreferrer")}>{IconLaunch}</li>}
                     </ul>
                 </div>
                 <p className="project_description">
