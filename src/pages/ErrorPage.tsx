94defbc34ad88f6d5b7dc9bd75683232ff6f2b2f
Yusuf <yusufsaquib@gmail.com>
2021-07-10 01:26:08 -0400

    made blog and error404 look nicer with flex

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -5,18 +5,17 @@
 
 import React, { FC } from 'react';
 
-import { Container } from '@material-ui/core';
 import PageNotFound from '../components/layout/PageNotFound';
 import Head from '../components/layout/Head';
  
 const Error404 : FC = () =>
 {
     return (
-        <Container>
+        <>
             <Head title="Page Not Found" />
 
             <PageNotFound />
-        </Container>
+        </>
     );
 }
 
