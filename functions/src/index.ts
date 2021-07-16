b263b15509be275234999cc68bc377f2a14373f4
Yusuf <yusufsaquib@gmail.com>
2021-07-16 00:29:26 -0400

    updated functions to hide api key

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,21 +10,14 @@ import axios from "axios";
 // });
 
 exports.sendRecaptcha = functions.https.onRequest(async (req, res) => {
-    res.set('Access-Control-Allow-Origin', "localhost");
-  
-    const secret = "<SECRET RECAPTCHA API KEY>";
-  
-    //Front-end will send the token        
-    const token = req.query.token;
-    const response = await axios.get(`https://recaptcha.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`);
-    const data = response.data;
-  
-    if (data.success) {
-        // Send the score back
-        res.status(200).send({ success: true, score: data.score });
-    }
-    else
-    {
-        res.status(200).send({success: false, score: 0 });
-    }
+  res.set("Access-Control-Allow-Origin", "https://www.yusufsaquib.com/");
+  const secret = functions.config().recaptcha.secret;
+  const token = req.query.token;
+  const response = await axios.get(`https://recaptcha.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`);
+  const data = response.data;
+  if (data.success) {
+    res.status(200).send({success: true, score: data.score});
+  } else {
+    res.status(200).send({success: false, score: 0});
+  }
 });
