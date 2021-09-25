d0e230892549049e87708ecff341a60e35ea5877
Yusuf Saquib <yusufsaquib@gmail.com>
2021-09-25 19:01:09 +0300

    started career item

--- src/components/elements/CareerItem.tsx
+++ src/components/elements/CareerItem.tsx
@@ -6,6 +6,7 @@
 import { format } from 'date-fns';
 import React, {FC} from 'react';
 import { CareerData } from '../../store/types/careerTypes';
+import { IconAt } from './Icons';
 
 
 interface CareerProps extends CareerData
@@ -17,7 +18,11 @@ const CareerItem : FC<CareerProps> = ({className, ...careerData}) =>
 {
     
     return (
-        <div>
+        <div id={`js-${careerData.career_id}`} className={`career_item_wrapper`}>
+            <h2 className="career_item_title">Software Engineer</h2>
+            <h2 className="career_item_location svg_icon">{IconAt}</h2>
+            <h2 className="career_item_location">Apple</h2>
+        
         </div>
     );
 }
--- src/components/sections/Work.tsx
+++ src/components/sections/Work.tsx
@@ -4,6 +4,7 @@
  */
 
 import React, { FC } from 'react'
+import CareerItem from '../elements/CareerItem';
 import Section from '../elements/Section';
 
 const Work : FC = () =>
@@ -11,7 +12,7 @@ const Work : FC = () =>
     
     return (
        <Section id="work" title="Work Experience">
-           
+           <CareerItem career_id="fd" career_title="Software Engineer" career_location="Apple" career_locationURL="www.apple.com" career_description={`Workd on some stuff\nAndmore stuff`} career_startDate={new Date(Date.now())} career_endDate={new Date(Date.now())}/>
        </Section>
     );
 }
