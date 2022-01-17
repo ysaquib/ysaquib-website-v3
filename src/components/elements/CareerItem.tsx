5283072abdb964cfb6b178c16a599a76449904c1
Yusuf Saquib <yusufsaquib@gmail.com>
2022-01-17 23:56:25 +0300

    udpated career item

--- src/components/elements/CareerItem.tsx
+++ src/components/elements/CareerItem.tsx
@@ -19,9 +19,19 @@ const CareerItem : FC<CareerProps> = ({className, ...careerData}) =>
     
     return (
         <div id={`js-${careerData.career_id}`} className={`career_item_wrapper`}>
-            <h2 className="career_item_title">Software Engineer</h2>
-            <h2 className="career_item_location svg_icon">{IconAt}</h2>
-            <h2 className="career_item_location">Apple</h2>
+            <div id={`js-${careerData.career_id}-details`} className={`career_item_details`}>
+                <h2 className="career_item_title">Research Assistant</h2>
+                <h2 className="career_item_location"><span className='svg_icon'>{IconAt}</span><span>Carnegie Mellon University</span></h2>
+            </div>
+            <div className="career_vert_line" />
+            <div id={`js-${careerData.career_id}-description`} className={`career_item_description`}>
+                <ul>
+                    <li>
+                        Did some stuff involving research in IoT device security and some other cool stuff
+                    </li>
+
+                </ul>
+            </div>
         
         </div>
     );
