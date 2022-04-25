118ae5c9d2d12589a3f0f15c2c402b8e3fa69c7e
Yusuf Saquib <yusufsaquib@gmail.com>
2022-04-25 11:09:14 +0300

    minor changes + remove signin button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,6 +11,7 @@ import Head from '../components/layout/Head';
 import Banner from '../components/sections/Banner';
 import Education from '../components/sections/Education';
 import Projects from '../components/sections/Projects';
+import Research from '../components/sections/Research';
 import Security from '../components/sections/Security';
 import Work from '../components/sections/Work';
 import ACWrapper from '../components/wrappers/ACWrapper';
@@ -24,6 +25,7 @@ const HomePage : FC = () =>
             <Banner />
             <Projects projectsLimit={3} id="projects"/>
             <Work getData={true} itemLimit={3} showButton/>
+            <Research getData={true} itemLimit={3} showButton/>
             <Education getData={true}/>
             <ACWrapper />
             <Security />
